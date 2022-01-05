b9200aa430488b3cbbac905a6d2e01d0d49a3a9d
Amr Hassanien <amr.m.hassanien@gmail.com>
2022-01-05 14:50:34 +0400

    CRNK API
    Adding CRNK JSON API
    
    - Dependencies and CRNK BOM are added to pom.xml
    
    - Excluding security auto configuration from boot application
    
    -  Adding CRNK configuration to application.properties

--- src/main/java/com/example/myblog/MyblogApplication.java
+++ src/main/java/com/example/myblog/MyblogApplication.java
@@ -3,7 +3,9 @@ package com.example.myblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Excluding SecurityAutoConfiguration and UserDetailsServiceAutoConfiguration is required to disable security
+//another exclusion is required if actuator is added in the dependency ( ManagementWebSecurityAutoConfiguration )
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class, org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class})
 public class MyblogApplication {
 
 	public static void main(String[] args) {
